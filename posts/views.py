51584f84e2b0861b4427d29f81b30331230daa88
Sofya <sofya.s.frolova@gmail.com>
2021-01-23 19:35:20 +0300

    Changings after PEP8 tests2

--- posts/views.py
+++ posts/views.py
@@ -2,22 +2,25 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 from .models import Post, Group
 
+
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
     return render(request, "index.html", {"posts": latest})
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
 
+
 def new_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
-        if form.is_valid(): 
+        if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            form.save()           
+            form.save()
             return redirect('index')
         return render(request, 'new.html', {'form': form})
     form = PostForm()
--- yatube/urls.py
+++ yatube/urls.py
@@ -17,11 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-        #  регистрация и авторизация
     path("auth/", include("users.urls")),
-
-    #  если нужного шаблона для /auth не нашлось в файле users.urls — 
-    #  ищем совпадения в файле django.contrib.auth.urls
     path("auth/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("", include("posts.urls")),
